b64a90f08b7ba87c9ea9e9545a4f1879fe8fd636
ByeongChang <als6068@naver.com>
2024-03-27 20:27:46 +0900

    Refactor - change some codes
    유튜브 링크로 spleeter 변환 후 다운받기 위한 코드를 추가합니다.

--- PyServer/main.py
+++ PyServer/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
 from youtube_download import youtube_download
-app = FastAPI()
+from pydub import AudioSegment
+import subprocess
+import pandas as pd
+import os
 
-@app.get("/")
-async def root():
-    youtube_download("https://www.youtube.com/watch?v=ayREauImhZg&ab_channel=%EC%9B%85%ED%82%A4")
-    return {"message": "Hello World"}
+AudioSegment.converter ="./.venv/ffmpeg/bin/ffmpeg.exe"
+
+AudioSegment.ffprobe = "./.venv/ffmpeg/bin/ffprobe.exe"
+
+links = pd.read_csv("./youtubeLink.csv")
+for data in links.values:
+    
+    title,channel,url = data[0], data[1],data[2]
+    result = youtube_download(title, url)
+    print(title)
+    if result:
+        command = ["spleeter", "separate", "-p", "spleeter:5stems","-o", "music", f"./music/{title}"]
+        subprocess.run(command, shell=True)
+        os.remove(f"./music/{title}.mp3")
+
+# from pydub.utils import mediainfo
+# from pathlib import Path
+#     def mp3_to_wav(mp3_file, wav_file):
+#     sound = AudioSegment.from_mp3(mp3_file)
+#     sound.export(wav_file, format="wav")
+
+# if __name__ == "__main__":
+#     mp3_file = Path("./haeyo.mp3")
+#     wav_file = "output_wav_file.wav"
+#     mp3_to_wav(mp3_file, wav_file)
