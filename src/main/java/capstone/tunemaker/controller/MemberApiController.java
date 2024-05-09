04fb16f4a55b1d7fee8688a6091c5d1d71dda827
HeoJunHyoung <gjwnsgud4016@ajou.ac.kr>
2024-05-09 22:11:51 +0900

    Add MemberApiController and CreateMemberRequest - Added MemberApiController class for handling member registration requests - Added CreateMemberRequest class for representing member registration data

---
+++ src/main/java/capstone/tunemaker/controller/MemberApiController.java
@@ -0,0 +1,30 @@
+package capstone.tunemaker.controller;
+
+import capstone.tunemaker.dto.CreateMemberRequest;
+import capstone.tunemaker.entity.Member;
+import capstone.tunemaker.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public String joinMember(@RequestBody @Validated CreateMemberRequest request){
+
+        if (!request.getPassword1().equals(request.getPassword2())) {
+            throw new IllegalArgumentException("Passwords do not match");
+        }
+        memberService.join(request);
+        return "ok";
+    }
+
+
+
+}
---
+++ src/main/java/capstone/tunemaker/dto/CreateMemberRequest.java
@@ -0,0 +1,32 @@
+package capstone.tunemaker.dto;
+
+import capstone.tunemaker.enums.Gender;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateMemberRequest {
+
+    @NotEmpty
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$" , message = "닉네임은 특수문자를 포함하지 않은 2~10자리여야 합니다.")
+    private String nickname;
+
+    @NotEmpty
+    @Size(min=8)
+    @Email
+    private String username;
+
+    @NotEmpty
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    private String password1;
+
+    @NotEmpty
+    private String password2;
+
+    @NotNull
+    private Gender gender;
+
+}
