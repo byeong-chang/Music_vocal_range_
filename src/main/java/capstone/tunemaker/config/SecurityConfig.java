1775ba8ba03cf24419a13637046c847975c56246
HeoJunHyoung <gjwnsgud4016@ajou.ac.kr>
2024-05-09 23:38:56 +0900

    feat : Add CORS configuration in SecurityConfig - Added CORS configuration to allow request from specifed origins url

--- src/main/java/capstone/tunemaker/config/SecurityConfig.java
+++ src/main/java/capstone/tunemaker/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package capstone.tunemaker.config;
 import capstone.tunemaker.jwt.JWTFilter;
 import capstone.tunemaker.jwt.JWTUtil;
 import capstone.tunemaker.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -52,25 +60,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
-        http.csrf((auth) -> auth.disable());
+        http.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
 
-        http.formLogin((auth) -> auth.disable());
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "http://localhost:8080", "http://127.0.0.1:8000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
 
-        http.httpBasic((auth) -> auth.disable());
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
-        http.authorizeHttpRequests((auth) -> auth
+                        return configuration;
+                    }
+                }))
+                .csrf((auth) -> auth.disable())
+                .formLogin((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable())
+                .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                );
-
-        http.sessionManagement((session) -> session
+                )
+                .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
-
-        http.addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
-
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+                )
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
