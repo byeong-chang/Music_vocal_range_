e87c02ea5abb373343e61bc282036e36e9b62856
HeoJunHyoung <gjwnsgud4016@ajou.ac.kr>
2024-05-10 19:34:53 +0900

    fix: Corrected the parameter in findByUrlId method
    In the MusicRepository class, the findByUrlId method was previously searching for Music entities based on the 'url' field. This was causing errors as the method should have been searching based on the 'urlId' field. This commit corrects the parameter in the findByUrlId method from 'url' to 'urlId', fixing the issue.

--- src/main/java/capstone/tunemaker/repository/MusicRepository.java
+++ src/main/java/capstone/tunemaker/repository/MusicRepository.java
@@ -18,9 +18,9 @@ public class MusicRepository {
 
     // findOne으로 return em.find(Music.class, id);를 못하는 이유는 클라이언트가 넘겨주는 값(Url)이 정상인지 비정상인지 확인하기 전이라
     // 함부로 Music 테이블에 값을 저장하면서 id값을 확인할 수가 없음
-    public Music findByUrlId(String url){
-        return em.createQuery("select m from Music as m where m.url=:url", Music.class)
-                .setParameter("url", url)
+    public Music findByUrlId(String urlId){
+        return em.createQuery("select m from Music as m where m.urlId=:urlId", Music.class)
+                .setParameter("urlId", urlId)
                 .getSingleResult();
     }
 }
