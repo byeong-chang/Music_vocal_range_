1ebae00fe96d0e8eadc22dbe7fb8933d6a254176
HeoJunHyoung <gjwnsgud4016@ajou.ac.kr>
2024-05-14 12:56:24 +0900

    feat: Add logout endpoint to MemberApiController
    This commit introduces a new GET endpoint at '/admin/logout' in the MemberApiController. This endpoint handles logout requests by adding the user's JWT token to a blacklist, effectively invalidating the token for future requests. A helper endpoint at '/logoutlist' is also added for debugging purposes, which prints the current state of the token blacklist.

--- src/main/java/capstone/tunemaker/controller/MemberApiController.java
+++ src/main/java/capstone/tunemaker/controller/MemberApiController.java
@@ -1,9 +1,14 @@
 package capstone.tunemaker.controller;
 
 import capstone.tunemaker.dto.CreateMemberRequest;
+import capstone.tunemaker.dto.CustomMemberDetails;
 import capstone.tunemaker.entity.Member;
+import capstone.tunemaker.jwt.JWTUtil;
 import capstone.tunemaker.service.MemberService;
+import capstone.tunemaker.service.TokenBlacklistService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberApiController {
 
+    private  final TokenBlacklistService tokenBlacklistService;
     private final MemberService memberService;
 
     @PostMapping("/join")
@@ -24,5 +30,20 @@ public class MemberApiController {
         }
         memberService.join(request);
     }
+
+    @GetMapping("/admin/logout")
+    public void logout(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            String token = authorization.split(" ")[1];
+            tokenBlacklistService.blacklist(token);
+        }
+    }
+
+    @GetMapping("/logoutlist")
+    public void logoutList(){
+        tokenBlacklistService.printBlacklist();
+    }
+
 
 }
