0b099acf6bb539a0ae8d8d69023943f479468f48
HeoJunHyoung <gjwnsgud4016@ajou.ac.kr>
2024-05-14 12:55:48 +0900

    feat: Add TokenBlacklistService for JWT token management
    This commit introduces the TokenBlacklistService class, which provides functionality for managing a blacklist of JWT tokens. This service allows tokens to be added to the blacklist, checks if a token is in the blacklist, and removes expired tokens from the blacklist at midnight every day. It also includes a method to print the current state of the blacklist for debugging purposes.

---
+++ src/main/java/capstone/tunemaker/service/TokenBlacklistService.java
@@ -0,0 +1,44 @@
+package capstone.tunemaker.service;
+
+import capstone.tunemaker.jwt.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TokenBlacklistService {
+
+    private final Map<String, String> blacklist = new ConcurrentHashMap<>();
+    private final JWTUtil jwtUtil;
+
+    public void blacklist(String token){
+        blacklist.put(token, "");
+    }
+
+    public boolean isBlacklisted(String token) {
+        return blacklist.containsKey(token);
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void removeExpiredTokens() {
+        for (String token : blacklist.keySet()) {
+            if (jwtUtil.isExpired(token)) {
+                blacklist.remove(token);
+            }
+        }
+    }
+
+    public void printBlacklist(){
+        for (Map.Entry<String, String> stringStringEntry : blacklist.entrySet()) {
+            log.error("stringStringEntry.getKey() = {}",  stringStringEntry.getKey());
+            log.error("stringStringEntry.getValue() = {}",  stringStringEntry.getValue());
+        }
+    }
+
+}
